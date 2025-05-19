bc0b18ae3b1d44ccfeb2e82e2362298f21845a15
mitajannahs <miftajannah0305@gmail.com>
2025-05-19 15:08:11 +0700

    menambahkan void sisip film

--- project.cpp
+++ project.cpp
@@ -23,6 +23,49 @@ bool listKosong()
     return (awal == nullptr);
 }
 
+void sisipFilm(char judulBaru[100], int tahun, char genreBaru[50], float ratingBaru)
+{
+
+    Film *NB = new Film;
+    strcpy(NB->judul, judulBaru);
+    NB->tahunRilis = tahun;
+    strcpy(NB->genre, genreBaru);
+    NB->rating = ratingBaru;
+    NB->kanan = nullptr;
+    NB->kiri = nullptr;
+
+    if (listKosong())
+    {
+        awal = NB;
+        akhir = NB;
+    }
+    else if (tahun <= awal->tahunRilis)
+    {
+        NB->kanan = awal;
+        awal->kiri = NB;
+        awal = NB;
+    }
+    else
+    {
+        Film *bantu = awal;
+        while (bantu->kanan != nullptr && tahun > bantu->kanan->tahunRilis)
+        {
+            bantu = bantu->kanan;
+        }
+        NB->kanan = bantu->kanan;
+        if (bantu->kanan != nullptr)
+        {
+            bantu->kanan->kiri = NB;
+        }
+        else
+        {
+            akhir = NB;
+        }
+        NB->kiri = bantu;
+        bantu->kanan = NB;
+    }
+}
+
 int main (){
 
 
