eac5055aa6787b605929fb59dbb6cf6b900e34f4
Fhixx <17taufiknh@gmail.com>
2025-05-19 15:17:12 +0700

    menambahkan void buatlist dan void hapus

--- project.cpp
+++ project.cpp
@@ -66,6 +66,92 @@ void sisipFilm(char judulBaru[100], int tahun, char genreBaru[50], float ratingB
     }
 }
 
+void buatListBaru()
+{
+    // Hapus semua node yang ada di list
+    while (awal != nullptr)
+    {
+        Film *hapus = awal;
+        awal = awal->kanan;
+        delete hapus;
+    }
+    akhir = nullptr;
+
+    // Baca ulang dari file dan isi list
+    FILE *file = fopen("FavoFilm.dat", "r");
+    if (!file)
+    {
+        cout << "Gagal membuka file untuk inisialisasi list.\n";
+        return;
+    }
+
+    char judul[100], genre[50];
+    int tahun;
+    float rating;
+
+    while (fscanf(file, "%[^;];%d;%[^;];%f\n", judul, &tahun, genre, &rating) == 4)
+    {
+        sisipFilm(judul, tahun, genre, rating);
+    }
+
+    fclose(file);
+}
+
+void hapusFilm(char judulHapus[100])
+{
+    if (listKosong())
+    {
+        cout << "List masih kosong.\n";
+        return;
+    }
+
+    if (awal == akhir && strcmp(awal->judul, judulHapus) == 0)
+    {
+        delete awal;
+        awal = nullptr;
+        akhir = nullptr;
+        cout << "Film \"" << judulHapus << "\" berhasil dihapus.\n";
+        return;
+    }
+
+    if (strcmp(awal->judul, judulHapus) == 0)
+    {
+        Film *hapus = awal;
+        awal = awal->kanan;
+        if (awal != nullptr)
+            awal->kiri = nullptr;
+        delete hapus;
+        cout << "Film \"" << judulHapus << "\" berhasil dihapus.\n";
+        return;
+    }
+
+    Film *bantu = awal;
+    while (bantu->kanan != nullptr && strcmp(bantu->kanan->judul, judulHapus) != 0)
+    {
+        bantu = bantu->kanan;
+    }
+
+    if (bantu->kanan != nullptr && strcmp(bantu->kanan->judul, judulHapus) == 0)
+    {
+        Film *hapus = bantu->kanan;
+        bantu->kanan = hapus->kanan;
+        if (hapus->kanan != nullptr)
+        {
+            hapus->kanan->kiri = bantu;
+        }
+        else
+        {
+            akhir = bantu;
+        }
+        delete hapus;
+        cout << "Film \"" << judulHapus << "\" berhasil dihapus.\n";
+    }
+    else
+    {
+        cout << "Film dengan judul \"" << judulHapus << "\" tidak ditemukan.\n";
+    }
+}
+
 int main (){
 
 
